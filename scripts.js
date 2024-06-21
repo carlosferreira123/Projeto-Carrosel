f756cb3bcf46a377d468ea93510d62310bbf0215
carlosferreira123 <122993323+carlosferreira123@users.noreply.github.com>
2024-06-21 18:22:15 -0300

    Atualização

--- scripts.js
+++ scripts.js
@@ -10,13 +10,26 @@ btnBack.onclick = () => moveItemsOnClick('back')
 
 function moveItemsOnClick(type) {
     let listItems = document.querySelectorAll('.list .list-item')
-    let thumItems = document.querySelectorAll('.list .list-item')
+    let thumbItems = document.querySelectorAll('.thumb .thumb-item')
 
+   console.log(listItems)
+   console.log(thumbItems)
+   
+   
     if(type === 'next') {
         list.appendChild(listItems[0])
         thumb.appendChild(thumbItens[0])
+        container.classList.add('next')
     } else {
+        list.prepend(listItems [listItems.length -1])
+        thumb.prepend(thumbItems [listItems.length -1])
+        container.classList.add('back')
 
     }
 
+    setTimeout(() => {
+        container.classList.remove('next')
+        container.classList.remove('back')
+
+      }  , 3000);
 }
